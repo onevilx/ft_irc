79d8254109472ad48c2444b984869b81a9f7d429
Otman Bouftoug <obouftou@e2r2p1.1337.ma>
2026-02-07 23:34:29 +0100

    to laptop

--- headers/channel.hpp
+++ headers/channel.hpp
@@ -10,7 +10,15 @@ class Client;
 class channel
 {
     std::string Cname; // there are some conditions to respect in the naming phase
-    std::vector<std::string>  ddfd;// this would be the users insde a channel 
+    std::vector<Client *>  Clients;// this would be the users insde a channel
+    // here i will figure if i need to store moderstors in a single vector or seek another apraoch 
+    // we'll figure it later
+    std::string topic;
+    std::string topic_setter;
+    std::string topic_time;
+    std::string channelTime; //to store the time which the channel got created 
+
+    int moderators; // here i will track the number of moderator in oe channel
     
 };
 
