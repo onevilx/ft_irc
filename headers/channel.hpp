983a1b1c49004fa3e32fa4e6a656bb97244d262d
OthmanBOF <bouftoukothman@gmail.com>
2026-02-08 02:32:49 +0100

    channel class hals designed

--- headers/channel.hpp
+++ headers/channel.hpp
@@ -7,8 +7,9 @@
 // here ineed a client class 
 class Client;
 
-class channel
+class Channel
 {
+    private:
     std::string Cname; // there are some conditions to respect in the naming phase
     std::vector<Client *>  Clients;// this would be the users insde a channel
     // here i will figure if i need to store moderstors in a single vector or seek another apraoch 
@@ -20,6 +21,59 @@ class channel
 
     int moderators; // here i will track the number of moderator in oe channel
     
+
+    //channel features
+    bool i; // inv 
+    bool k; //
+    std::string key;
+    bool l;// limit a jmmi
+    bool t;//topic restrict
+
+    size_t limit; // this will be trigred if the l is setted as true 
+
+    public:
+    // canonical form 
+    Channel();
+    Channel(std::string name, std::string key);
+    Channel(const Channel& copy);
+    ~Channel();
+
+    //setters to true 
+    void set_i_on();
+    void set_k_on();
+    void set_k(std::string Val);
+    void set_l_on();
+    void set_t_on();
+
+    void set_i_off();
+    void set_k_off();
+    void set_k_off();
+    void set_l_off();
+    void set_t_off();
+    
+
+    // normal setters
+    std::string set_Cname();
+    std::string set_topic();
+    std::string set_topic_setter();
+    std::string set_topic_time();
+    std::string set_Ctime();
+
+    // getters
+    void get_k();
+    void get_i();
+    void get_l();
+    void get_l();
+    std::string get_Cname();
+    std::string get_topic();
+    std::string get_topic_setter();
+    std::string get_topic_time();
+    std::string get_Ctime();
+
+
+
+
+
 };
 
 
